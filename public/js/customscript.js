ab207f495d6870690f724ed82b7d7c50ed99930d
Patricia <patriciasteffen@hotmail.com>
2023-04-30 10:45:51 +0200

    links

--- public/js/customscript.js
+++ public/js/customscript.js
@@ -11,7 +11,9 @@ createApp({
           title: 'Klang23 das Musikfestival',
           description: 'Mit Esther Lenherr am Klavier.',
           when: 'Samstag 17. Juni 2023, 17 Uhr',
+          costshtml: '<a href="www.google.ch" target=”_blank”>LINK</a>', 
           costs: 'Infos folgen',
+          link: '',
           where: 'Reformierte Kirche Wädenswil',
           img: './assets/img/concerts/smaller/klang_600.jpg',
           imgalt: 'Foto vom Klang 2019'
@@ -20,7 +22,8 @@ createApp({
           title: 'Chornacht im Gaswerk',
           description: 'Mit Vocabular und dem Männerchor Schwyz',
           when: 'Freitag 1. September 2023, 20 Uhr',
-          costs: 'Infos folgen', 
+          costshtml: '<a href="www.google.ch" target=”_blank”>LINK</a>', 
+          link: 'https://www.gaswerk-eventbar.ch/veranstaltungen/nacht-der-choere/',
           where: 'Gaswerk Seewen',
           img: './assets/img/concerts/smaller/kulturschock2_600copy.jpg',
           imgalt: 'Foto vom Kulturschock im Gaswerk'
@@ -29,7 +32,9 @@ createApp({
           title: 'Muotitaler Alpchäsmärcht',
           description: 'Mit Andrea Ulrich am Akkordeon, Gaby Näf am Klavier und Monika Greenwald am Cello',
           when: 'Samstag 28. Oktober 2023 ab 20 Uhr',
+          costshtml: '<a href="www.google.ch" target=”_blank”>LINK</a>', 
           costs: 'Infos folgen',
+          link: '',
           where: 'Muotathal',
           img: './assets/img/concerts/smaller/muotathal.jpg',
           imgalt: 'Foto Käsemarkt'
