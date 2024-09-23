1000a81c15f74ff216dcc41f925ebd953f0c3506
Patricia Offline <pat575859@gmail.com>
2024-09-23 15:50:53 +0200

    facebook weg

--- public/js/customscript.js
+++ public/js/customscript.js
@@ -1,76 +1,76 @@
-document.addEventListener('click', showShamrock);
+// document.addEventListener('click', showShamrock);
 
-function showShamrock(event) {
-  var posX = event.pageX;
-  var posY = event.pageY;
+// function showShamrock(event) {
+//   var posX = event.pageX;
+//   var posY = event.pageY;
 
-  createShamrock(posX, posY, randomShamrock(), randomClass(), randomGreen());
+//   createShamrock(posX, posY, randomShamrock(), randomClass(), randomGreen());
 
 
-  var svgs = document.getElementsByTagName('svg');
-  if (svgs.length > 50) {
-    for (i = svgs.length - 1; i >= 0; i--) {
-      svgs[i].parentNode.removeChild(svgs[i]);
-    }
-  }
-
-}
-
-function randomGreen() {
-  var greens = ['#66a841', '#7ba237', '#95eb00', '#00b842', '#4caf50', '#56B949', '#C1CCAE', '#03d1ab', '#8DC0B1', '#B2EDCE', '#88C542', '#a6cb62', '#47cb4a', '#9BF14F', '#6b7d27', '#4caf50', '#00be70', '#207720', '#86c036'];
-  var random = Math.floor(Math.random() * (greens.length - 1 + 1)) + 0;
-  var green = greens[random];
-  // console.log(random, green);
-  return green;
-}
-
-function randomClass() {
-  var animClass = ['normal-fade', 'counter-fade'];
-  var random = Math.floor(Math.random() * (animClass.length - 1 + 1)) + 0;
-  return animClass[random];
-}
-
-function randomShamrock() {
-  var shamrockList = [
-    {
-      'width': 111.994,
-      'height': 110.637,
-      'viewBox': "0 0 111.994 110.637",
-      'd': 'M111.462,65.159c2.263-13.032-15.935-19.88-25.183-21.101c11.188-11.163,15.355-38.708-8.668-31.884c-7.935-27.668-30.969-1.793-32.394,14.748C34.472,15.305,4.875,9,12.046,34.245c-26.144,7.497-3.973,29.846,12.314,32.113C13.096,77.463,8.66,105.296,32.845,98.426c8.066,28.13,31.356,1.1,32.455-15.587c6.062,6.934,21.552,18.087,31.092,9.219c3.879-4.685,4.791-9.955,2.736-15.811C105.424,74.862,109.536,71.167,111.462,65.159C113.023,56.169,110.558,70.376,111.462,65.159z'
-    },
-    {
-      'width': 215.864,
-      'height': 234.908,
-      'viewBox': "0 0 215.864 234.908",
-      'd': "M215.141,117.341c3.291-25.531-30.435-37.346-50.109-36.702c20.565-21.12,21.139-82.684-19.291-54.5c-8.249-53.786-59.09-13.651-59.853,22.91C65.84,22.042,19.937,19.389,31.183,63.042c-58.854-1.868-23.982,66.436,19.115,67.457c-31.552,25.345-29.373,50.841,8.254,55.072c-3.511,55.13,52.971,18.657,59.89-14.906c7.204,19.541-15.156,42.589-9.872,57.707c7.312,20.926,20.941-15.131,20.858-14.653c3.351-19.336-1.521-36.422,0.143-54.847c15.044-2.052,41.946,45.009,63.252,14.188c7.172-10.371,1.271-22.031-1.28-32.563C202.365,137.243,213.57,129.531,215.141,117.341C217.319,100.446,213.747,128.157,215.141,117.341z"
-    },
-  ];
-
-  var random = Math.floor(Math.random() * (shamrockList.length - 1 + 1)) + 0;
-  return shamrockList[random];
-}
-
-function createShamrock(x, y, shape, animation, fill) {
-  var shamrock = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  shamrock.setAttribute('width', shape.width + 'px');
-  shamrock.setAttribute('height', shape.height + 'px');
-  shamrock.setAttribute('class', animation);
-  shamrock.setAttribute('viewBox', shape.viewBox);
-  shamrock.setAttribute('fill', fill);
-  shamrock.style.position = 'absolute';
-  shamrock.style.top = (y - (shape.height / 2)) + 'px';
-  shamrock.style.left = (x - (shape.width / 2)) + 'px';
-  var svgNS = shamrock.namespaceURI;
-  var path = document.createElementNS(svgNS, 'path');
-  path.setAttribute('d', shape.d);
-  shamrock.appendChild(path);
-  document.body.appendChild(shamrock);
-}
-
-// function showDOM (){
 //   var svgs = document.getElementsByTagName('svg');
-//   console.log(svgs);
+//   if (svgs.length > 50) {
+//     for (i = svgs.length - 1; i >= 0; i--) {
+//       svgs[i].parentNode.removeChild(svgs[i]);
+//     }
+//   }
+
 // }
+
+// function randomGreen() {
+//   var greens = ['#66a841', '#7ba237', '#95eb00', '#00b842', '#4caf50', '#56B949', '#C1CCAE', '#03d1ab', '#8DC0B1', '#B2EDCE', '#88C542', '#a6cb62', '#47cb4a', '#9BF14F', '#6b7d27', '#4caf50', '#00be70', '#207720', '#86c036'];
+//   var random = Math.floor(Math.random() * (greens.length - 1 + 1)) + 0;
+//   var green = greens[random];
+//   // console.log(random, green);
+//   return green;
+// }
+
+// function randomClass() {
+//   var animClass = ['normal-fade', 'counter-fade'];
+//   var random = Math.floor(Math.random() * (animClass.length - 1 + 1)) + 0;
+//   return animClass[random];
+// }
+
+// function randomShamrock() {
+//   var shamrockList = [
+//     {
+//       'width': 111.994,
+//       'height': 110.637,
+//       'viewBox': "0 0 111.994 110.637",
+//       'd': 'M111.462,65.159c2.263-13.032-15.935-19.88-25.183-21.101c11.188-11.163,15.355-38.708-8.668-31.884c-7.935-27.668-30.969-1.793-32.394,14.748C34.472,15.305,4.875,9,12.046,34.245c-26.144,7.497-3.973,29.846,12.314,32.113C13.096,77.463,8.66,105.296,32.845,98.426c8.066,28.13,31.356,1.1,32.455-15.587c6.062,6.934,21.552,18.087,31.092,9.219c3.879-4.685,4.791-9.955,2.736-15.811C105.424,74.862,109.536,71.167,111.462,65.159C113.023,56.169,110.558,70.376,111.462,65.159z'
+//     },
+//     {
+//       'width': 215.864,
+//       'height': 234.908,
+//       'viewBox': "0 0 215.864 234.908",
+//       'd': "M215.141,117.341c3.291-25.531-30.435-37.346-50.109-36.702c20.565-21.12,21.139-82.684-19.291-54.5c-8.249-53.786-59.09-13.651-59.853,22.91C65.84,22.042,19.937,19.389,31.183,63.042c-58.854-1.868-23.982,66.436,19.115,67.457c-31.552,25.345-29.373,50.841,8.254,55.072c-3.511,55.13,52.971,18.657,59.89-14.906c7.204,19.541-15.156,42.589-9.872,57.707c7.312,20.926,20.941-15.131,20.858-14.653c3.351-19.336-1.521-36.422,0.143-54.847c15.044-2.052,41.946,45.009,63.252,14.188c7.172-10.371,1.271-22.031-1.28-32.563C202.365,137.243,213.57,129.531,215.141,117.341C217.319,100.446,213.747,128.157,215.141,117.341z"
+//     },
+//   ];
+
+//   var random = Math.floor(Math.random() * (shamrockList.length - 1 + 1)) + 0;
+//   return shamrockList[random];
+// }
+
+// function createShamrock(x, y, shape, animation, fill) {
+//   var shamrock = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+//   shamrock.setAttribute('width', shape.width + 'px');
+//   shamrock.setAttribute('height', shape.height + 'px');
+//   shamrock.setAttribute('class', animation);
+//   shamrock.setAttribute('viewBox', shape.viewBox);
+//   shamrock.setAttribute('fill', fill);
+//   shamrock.style.position = 'absolute';
+//   shamrock.style.top = (y - (shape.height / 2)) + 'px';
+//   shamrock.style.left = (x - (shape.width / 2)) + 'px';
+//   var svgNS = shamrock.namespaceURI;
+//   var path = document.createElementNS(svgNS, 'path');
+//   path.setAttribute('d', shape.d);
+//   shamrock.appendChild(path);
+//   document.body.appendChild(shamrock);
+// }
+
+// // function showDOM (){
+// //   var svgs = document.getElementsByTagName('svg');
+// //   console.log(svgs);
+// // }
 
 const { createApp } = Vue
 
